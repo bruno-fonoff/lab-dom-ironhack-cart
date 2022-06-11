14659fc0ec79c3bcdb7f0346d2aff3525869062f
bruno-fonoff <105615381+bruno-fonoff@users.noreply.github.com>
2022-06-11 18:51:48 -0300

    Solved lab

--- js/index.js
+++ js/index.js
@@ -3,21 +3,51 @@
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
+  const price = product.querySelector('.price span');
+
+  const quantity = product.querySelector('.quantity input');
+
+  let priceValue = price.innerText;
+  console.log(priceValue);
+
+  let quantityValue = quantity.value;
+  console.log(quantityValue);
+
+  let resultSubtotal = priceValue * quantityValue;
+  console.log(resultSubtotal);
+
+  let subtotal = product.querySelector('.subtotal span');
+  console.log(subtotal);
+
+  subtotal.innerText = resultSubtotal;
+
+  return resultSubtotal;
+
   //... your code goes here
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
   // ITERATION 2
   //... your code goes here
 
+  const products = document.getElementsByClassName('product');
+
+  //===>>>  consigo clonar, mas fica um ao lado do outro e nao embaixo   <<<===\\
+  //===>>>  fiz o clone direto no HTML para seguir em frente   <<<===\\
+
+  // let pclone = products[0].cloneNode(true);
+  // products[0].appendChild(pclone);
+
+  let totalValue = 0;
+
+  for (let product of products) {
+    totalValue += updateSubtotal(product);
+  }
+
   // ITERATION 3
   //... your code goes here
+
+  document.querySelector('#total-value span').innerText = totalValue;
 }
 
 // ITERATION 4
